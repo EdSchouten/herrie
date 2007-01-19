ff16ea5001900f9939ef9ad501a81b647e34ecce
Ed Schouten <ed@mekker.local>
2007-01-19 09:12:26 +0100

    Already add quite broken SDL audio output code.

---
+++ herrie/src/audio_output_sdl.c
@@ -0,0 +1,93 @@
+/*
+ * Copyright (c) 2006-2007 Ed Schouten <ed@fxq.nl>
+ * All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ */
+/**
+ * @file audio_output_sdl.c
+ */
+
+#include <SDL.h>
+#include <SDL_Audio.h>
+
+#include "audio_file.h"
+#include "audio_output.h"
+#include "gui.h"
+
+SDL_AudioSpec	curfmt;
+Uint8		buf[AUDIO_OUTPUT_BUFLEN];
+size_t		buflen;
+GCond		*bufcond;
+
+static void
+audio_output_write(void *data, Uint8 *stream, int len)
+{
+	if (buflen != 0) {
+		SDL_MixAudio(stream, buf, buflen, SDL_MIX_MAXVOLUME);
+		buflen = 0;
+	}
+}
+
+int
+audio_output_open(void)
+{
+	if (SDL_InitSubSystem(SDL_INIT_AUDIO) != 0)
+		/* XXX: show error */
+		return (1);
+
+	curfmt.format = AUDIO_S16LSB,
+	curfmt.size = AUDIO_OUTPUT_BUFLEN;
+	curfmt.callback = audio_output_write;
+
+	return (0);
+}
+
+int
+audio_output_play(struct audio_file *fd)
+{
+	if ((buflen = audio_file_read(fd, buf)) == 0)
+		return (0);
+
+	if (curfmt.freq != fd->srate || curfmt.channels != fd->channels) {
+		audio_output_close();
+
+		curfmt.freq = fd->srate;
+		curfmt.channels = fd->channels;
+
+		if (SDL_OpenAudio(&curfmt, NULL) != 0) {
+			gui_msgbar_warn(_("Cannot open the audio device."));
+			return (0);
+		}
+	}
+
+	SDL_PauseAudio(0);
+
+	//gui_msgbar_warn(_("Woot."));
+	return (buflen);
+}
+
+void
+audio_output_close(void)
+{
+	SDL_CloseAudio();
+}
