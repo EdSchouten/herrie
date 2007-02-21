d9b1bd33e6f4d85c189e0e5ac6fbc2781784e1c6
Ed Schouten <ed@flippo.fxq.nl>
2007-02-21 18:30:33 +0100

    Fix the Modplug code to do something already. :)

--- herrie/src/audio_format_modplug.c
+++ herrie/src/audio_format_modplug.c
@@ -27,27 +27,69 @@
  * @file audio_format_modplug.c
  */
 
+#include <sys/mman.h>
 #include <modplug.h>
 
 #include "audio_format.h"
 #include "audio_output.h"
 
+struct modplug_drv_data {
+	void		*map_base;
+	size_t		map_len;
+	ModPlugFile	*modplug;
+};
 
 int
 modplug_open(struct audio_file *fd)
 {
+	struct modplug_drv_data *data;
+
+	if (fd->stream) {
+		/* If only we could memory map the internet... */
+		return (-1);
+	}
+
+	fd->drv_data = data = g_slice_new(struct modplug_drv_data);
+
+	/* Calculate file length */
+	fseek(fd->fp, 0, SEEK_END);
+	data->map_len = ftello(fd->fp);
+
+	/* Memory map the entire file */
+	data->map_base = mmap(NULL, data->map_len, PROT_READ, 0,
+	    fileno(fd->fp), 0);
+	if (data->map_base == MAP_FAILED)
+		goto free;
+
+	/* Now feed it to modplug */
+	data->modplug = ModPlug_Load(data->map_base, data->map_len);
+	if (data->modplug != NULL) {
+		fd->time_len = ModPlug_GetLength(data->modplug) / 1000;
+		fd->tag.title = g_strdup(ModPlug_GetName(data->modplug));
+		return (0);
+	}
+
+	munmap(data->map_base, data->map_len);
+free:	g_slice_free(struct modplug_drv_data, data);
 	return (1);
 }
 
 void
 modplug_close(struct audio_file *fd)
 {
+	struct modplug_drv_data *data = fd->drv_data;
+
+	ModPlug_Unload(data->modplug);
+	munmap(data->map_base, data->map_len);
+	g_slice_free(struct modplug_drv_data, data);
 }
 
 size_t
 modplug_read(struct audio_file *fd, void *buf)
 {
-	return (0);
+	struct modplug_drv_data *data = fd->drv_data;
+
+	return ModPlug_Read(data->modplug, buf, AUDIO_OUTPUT_BUFLEN);
 }
 
 void
