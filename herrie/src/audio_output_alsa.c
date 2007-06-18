9b7529bb813a9a398757bb84c0685b6d95bbc2f4
Ed Schouten <ed@flippo.(none)>
2007-06-18 20:36:35 +0200

    First step in supporting multiple sample rates; split off the sample rate/channel setting to a separate function.

--- herrie/src/audio_output_alsa.c
+++ herrie/src/audio_output_alsa.c
@@ -46,46 +46,53 @@ static snd_pcm_t		*devhnd;
  *        ALSA.
  */
 static snd_pcm_hw_params_t	*devparam;
+static unsigned int		channels = 0;
+static unsigned int		srate = 0;
+
+static int
+audio_output_apply_hwparams(void)
+{
+	/* Sample rate */
+	if (snd_pcm_hw_params_set_rate_near(devhnd, devparam, &srate, NULL) != 0)
+		return (-1);
+	/* Channels */
+	if (snd_pcm_hw_params_set_channels(devhnd, devparam, channels) != 0)
+		return (-1);
+	/* Apply values */
+	if (snd_pcm_hw_params(devhnd, devparam) != 0)
+		return (-1);
+	/* And we're off! */
+	if (snd_pcm_prepare(devhnd) != 0)
+		return (-1);
+	
+	return (0);
+}
 
 int
 audio_output_open(void)
 {
-	int ret;
-	unsigned int srate = 44100;
-
 	/* Open the device */
-	if ((ret = snd_pcm_open(&devhnd,
+	if (snd_pcm_open(&devhnd,
 	    config_getopt("audio.output.alsa.device"),
-	    SND_PCM_STREAM_PLAYBACK, 0)) != 0)
+	    SND_PCM_STREAM_PLAYBACK, 0) != 0)
 		goto error;
 
 	/* Retreive the hardware parameters */
-	if ((ret = snd_pcm_hw_params_malloc(&devparam)) != 0)
+	if (snd_pcm_hw_params_malloc(&devparam) != 0)
 		goto error;
-	if ((ret = snd_pcm_hw_params_any(devhnd, devparam)) != 0)
+	if (snd_pcm_hw_params_any(devhnd, devparam) != 0)
 		goto error;
 
 	/* Set the access method - XXX: mmap */
-	if ((ret = snd_pcm_hw_params_set_access(devhnd, devparam,
-	    SND_PCM_ACCESS_RW_INTERLEAVED)) != 0)
+	if (snd_pcm_hw_params_set_access(devhnd, devparam,
+	    SND_PCM_ACCESS_RW_INTERLEAVED) != 0)
 		goto error;
 
-	/* XXX: We want 16 bits little endian, 2 channels, 44.1 KHz */
-	if (snd_pcm_hw_params_set_rate_near(devhnd, devparam, &srate,
-	    NULL) != 0)
-		goto error;
-	if ((ret = snd_pcm_hw_params_set_format(devhnd, devparam,
-	    SND_PCM_FORMAT_S16_LE)) != 0)
-		goto error;
-	if (snd_pcm_hw_params_set_channels(devhnd, devparam, 2) != 0)
-		goto error;
-	
-	/* Now apply the parameters and get started */
-	if (snd_pcm_hw_params(devhnd, devparam) != 0)
-		goto error;
-	if (snd_pcm_prepare(devhnd) != 0)
-		goto error;
-	
+	/* Output format */
+	if (snd_pcm_hw_params_set_format(devhnd, devparam,
+	    SND_PCM_FORMAT_S16_LE) != 0)
+		return (-1);
+
 	return (0);
 error:
 	g_printerr(_("Cannot open the audio device.\n"));
@@ -102,9 +109,18 @@ audio_output_play(struct audio_file *fd)
 	if ((len = audio_file_read(fd, buf, sizeof buf)) == 0)
 		return (-1);
 	
-	if (fd->channels != 2 || fd->srate != 44100) {
-		gui_msgbar_warn(_("Sample rate or amount of channels not supported."));
-		return (-1);
+	if (fd->channels != channels || fd->srate != srate) {
+		/* Reset the stream */
+		snd_pcm_drain(devhnd);
+		snd_pcm_drop(devhnd);
+
+		/* Apply the new values */
+		channels = fd->channels;
+		srate = fd->srate;
+		if (audio_output_apply_hwparams() != 0) {
+			gui_msgbar_warn(_("Sample rate or amount of channels not supported."));
+			return (-1);
+		}
 	}
 
 	/* ALSA measures in sample lengths */
