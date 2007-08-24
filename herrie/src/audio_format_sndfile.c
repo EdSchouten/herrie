17dd6556c2dc355f3c8d51164f46ea8a7c5215cd
Ed Schouten <ed@palm.hoeg.nl>
2007-08-24 11:34:07 +0200

    Final changes for the next release...

--- herrie/src/audio_format_sndfile.c
+++ herrie/src/audio_format_sndfile.c
@@ -63,7 +63,7 @@ sndfile_open(struct audio_file *fd, const char *ext)
 	/* Metadata - libsndfile only has artist + title */
 	fd->tag.artist = g_strdup(sf_get_string(hnd, SF_STR_ARTIST));
 	fd->tag.title = g_strdup(sf_get_string(hnd, SF_STR_TITLE));
-#ifdef notyet
+#if 0
 	fd->tag.album = g_strdup(sf_get_string(hnd, SF_STR_ALBUM));
 #endif
 
