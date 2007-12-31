428cd168aad667b70fdf0759e8da18113fc8f63c
Ed Schouten <ed@fxq.nl>
2007-12-31 09:58:38 +0100

    Bump the Copyright (C) year to 2008. :-)

--- misc/cdepcalc.c
+++ misc/cdepcalc.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2007 Ed Schouten <ed@fxq.nl>
+ * Copyright (c) 2006-2008 Ed Schouten <ed@fxq.nl>
  * All rights reserved.
  * 
  * Redistribution and use in source and binary forms, with or without
