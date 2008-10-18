f673661e2c1286c1fac5b45afb9b172fdfc23d19
Ed Schouten <ed@80386.nl>
2008-10-18 10:23:41 +0200

    Add a `fancy' directory listing for the distfiles to make bots work.

---
+++ www/distfiles/index.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+
+printf 'Content-type: text/html; charset=utf-8\n\n'
+
+cat << EOF
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+	<head>
+		<title>Herrie - distfiles</title>
+	</head>
+	<body>
+		<h1>Tarballs - bzip2</h1>
+		<ul>
+EOF
+
+for i in herrie-*.bz2
+do
+	printf '\t\t\t<li><a href="%s">%s</a></li>\n' $i $i
+done
+
+cat << EOF
+		</ul>
+
+		<h1>Tarballs - gzip</h1>
+		<ul>
+EOF
+
+for i in herrie-*.gz
+do
+	printf '\t\t\t<li><a href="%s">%s</a></li>\n' $i $i
+done
+
+cat << EOF
+		</ul>
+	</body>
+</html>
+EOF
